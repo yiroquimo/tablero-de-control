c28c8f63b73f6217341287888c7fdafbe5af1a36
yiroquimo <yiroquimo@gmail.com>
2025-09-10 09:01:49 -0500

    a las 9 am

--- main2.py
+++ main2.py
@@ -1,14 +1,35 @@
 import pandas as pd
 import streamlit as st
 
-url = 'https://github.com/juliandariogiraldoocampo/ia_taltech/raw/refs/heads/main/fiscalia/datos_generales_ficticios.xlsx'
-df = pd.read_csv(url, sep=';', encoding='utf-8')
+url = 'https://github.com/juliandariogiraldoocampo/ia_taltech/raw/refs/heads/main/fiscalia/datos_generales_ficticios.csv'
+df = pd.read_csv(url, sep=';', encoding='latin-1')
 
 st.dataframe(df)
 
 # crea lista de columnas de interes
-seleccion_columnas = ['FECHA_HECHOS'. 'DELITO', 'ETAPA','FISCAL_ASIGNADO', 'DEPARTAMENTO','MUNICIO_HECHOS']
+seleccion_columnas = ['FECHA_HECHOS', 'DELITO','ETAPA','FISCAL_ASIGNADO', 'DEPARTAMENTO','MUNICIPIO_HECHOS']
 #ACTUALIZO DATAFRAME CON LAS COLUMNAS DE INTERES, ORDENASD POR FECHA Y RESETEO EL INDICE
 df = df[seleccion_columnas].sort_values(by='FECHA_HECHOS', ascending=True).reset_index(drop=True)
 
+
+#convierto la columna fecha hechos a formato fecha
+df['FECHA_HECHOS']= pd.to_datetime(df['FECHA_HECHOS'], errors='coerce')
+
+# EXTRAIGO SOLO LA FECHA (SIN HORA)
+df['FECHA_HECHOS'] = df['FECHA_HECHOS'].dt.date
+
+st.dataframe(df)
+
+#CALCULO DE MUNICIPIOS QUE TIENEN MAS DELITOS, el value_ count cuanta cuantas veces se repite un dato
+
+max_municipio = df['MUNICIPIO_HECHOS'].value_counts().index[0]
+#max_municipio = df['MUNICIPIO_HECHOS'].value_counts().index[0].upper()
+st.write(f"Municipio TOP Delitos: {max_municipio}")
+
+st.write(max_municipio)
+
+# construir la pagina
+st.set_page_config(page_title="Tablero de control delitos fidcalia", layout="centered")
+st.header("Tablero de control Delitos fiscalia")
+#st.markdown(f"<center><h2>Tablero de control delitos fiscalia")
 st.dataframe(df)
